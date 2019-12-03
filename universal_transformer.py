79b2b4eaeba96f60791176b8753d4592afd803f9
Gabriel Marks <gabriel_marks@brown.edu>
2019-12-02 19:27:15 -0500

    add another layer

--- universal_transformer.py
+++ universal_transformer.py
@@ -23,7 +23,9 @@ class UniversalTransformer(Module):
 			out_seq_len, self.nheads, self.decoder_T, self.dropout, self.embedding_size)
 		self.enc_embedding_layer = nn.Embedding(in_vocab_len, self.embedding_size)
 		self.dec_embedding_layer = nn.Embedding(out_vocab_len, self.embedding_size)
-		self.ff_layer = nn.Linear(self.embedding_size, out_vocab_len)
+		self.ff_layer_1 = nn.Linear(self.embedding_size, 2048)
+		self.ff_layer_2 = nn.Linear(2048, out_vocab_len)
+		self.dropout_layer = nn.Dropout(self.dropout)
 
 		self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
 
@@ -32,7 +34,7 @@ class UniversalTransformer(Module):
 		enc_output = self.enc_layer(enc_embeddings)
 		dec_embeddings = self.dec_embedding_layer(decoder_input)
 		dec_output = self.dec_layer(dec_embeddings, enc_output)
-		return self.ff_layer(dec_output)
+		return self.ff_layer_2(self.dropout_layer(F.relu(self.ff_layer_1(dec_output))))
 
 class UniversalTransformerEncoder(Module):
 	def __init__(self, seq_len, nheads, T, dropout, emb_size, *args, **kwargs):
